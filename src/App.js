6d1fd9ddff2f75aec6a225cb2dee5ec8e47c348f
ivannebotov <everynomber@gmail.com>
2019-10-18 17:13:04 +0300

    Created Join us with Git utton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import  World from  './World';
+var GitHubForkRibbon = require('react-github-fork-ribbon'); 
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 
@@ -13,6 +14,12 @@ function App() {
       <Button>sdfsfs</Button>
       </Box>
       <World  width={500} height={500} />
+      <GitHubForkRibbon position="right" 
+                        color="green"
+                        href="//github.com/jessy1092/react-github-fork-ribbon" 
+                        target="_blank" > 
+        Fork me on GitHub 
+      </GitHubForkRibbon> 
     </div>
   );
 }
